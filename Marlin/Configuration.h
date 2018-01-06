ebcd48504e20603d6f85ead25ff9f5d26cc03318
Ivan Kalafatic <ikalafat@gmail.com>
2018-01-07 00:21:11 +0100

    z probe position, extruder steps adjustment for finer prints

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -524,7 +524,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 973 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 925 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -680,7 +680,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -25  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.85   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 6000
